f40e291e0a6a39d3d06f89327f9d0eb47b31d217
Kylian LE METTE <kylian.le.mette@gmail.com>
2018-04-27 09:07:24 +0200

    [DEL] Remove print in viewController

--- ios_attendance/Controllers/ViewController.swift
+++ ios_attendance/Controllers/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(Hash.SHA512("hello"))
     }
 
     override func didReceiveMemoryWarning() {
